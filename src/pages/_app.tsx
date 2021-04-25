27cc5089bf9eada4ed2696f806923b59b0d02147
Lucian Cardoso <luciancardoso20@gmail.com>
2021-04-25 19:01:45 -0400

    Add files via upload

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import '../styles/global.scss'
+import { Header } from '../components/Header'
+import { Player } from '../components/Player';
+
+import styles from '../styles/app.module.scss';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
+
+function MyApp({ Component, pageProps }) {
+  return(
+    <PlayerContextProvider>
+      <div className={styles.warpper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
+  )
+}
+
+export default MyApp
